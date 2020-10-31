ce5e6daf830017e48a561c4e822533952282796e
Rebecca <rhdai10@gmail.com>
2020-10-30 22:58:41 -0700

    add type definition

--- src/Main.elm
+++ src/Main.elm
@@ -5,24 +5,32 @@ import Html exposing (..)
 import Html.Events exposing (onClick)
 
 
+main : Program () Model Msg
 main =
     Browser.sandbox { view = view, update = update, init = init }
+
+
+type alias Model =
+    { showWinner : Bool }
 
 
 type Msg
     = Clicked
 
 
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         Clicked ->
             { showWinner = not model.showWinner }
 
 
+init : Model
 init =
     { showWinner = False }
 
 
+view : Model -> Html Msg
 view model =
     let
         winner =
